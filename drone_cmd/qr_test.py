58794cd8e129f137e247db56cc56631ec40e9a9c
Stefan Holdener <stefan.holdener@hotmail.com>
2016-09-17 03:50:06 +0200

    add qr test

---
+++ drone_cmd/qr_test.py
@@ -0,0 +1,17 @@
+"""
+from PIL import Image
+import zbarlight
+
+file_path = 'qr_code.jpg'
+with open(file_path, 'rb') as image_file:
+    image = Image.open(image_file)
+    image.load()
+
+codes = zbarlight.scan_codes('qrcode', image)
+print('QR codes: %s' % codes)
+"""
+
+from qrtools import QR
+myCode = QR(filename=u"qr_code.jpg")
+if myCode.decode():
+  print myCode.data
